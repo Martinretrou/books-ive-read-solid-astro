3a2899b29af8ef00585ecddc8e3948394f83a43d
RETROU Martin <martin_retrou@lm-jjwryg319q.groupevsc.com>
2024-09-06 15:37:03 +0200

    💄 Better responsive

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -18,20 +18,32 @@ export const Card: Component<BookProps> = (props) => {
   return (
     <div
       ref={props.ref}
-      class={"items-center grid sm:grid-cols-1 lg:grid-cols-23 group py-4 mb-4"}
+      class={"items-center grid sm:grid-cols-1 md:grid-cols-23 group py-4 mb-4"}
     >
-      <div class='flex flex-col lg:col-start-[12] lg:col-span-12'>
-        <img
-          class='lg:hidden flex w-[50vw] mb-4'
-          src={local.book.image.url}
-          alt={local.book.image.alt}
-        />
+      <div class='flex flex-col md:col-start-[12] md:col-span-12'>
+        <div class='relative'>
+          <img
+            class={`md:hidden relative flex w-[50vw] max-w-[260px] z-10 mb-4 transition-all ${
+              local.active
+                ? "scale-110 translate-x-3 -translate-y-3"
+                : "scale-100 "
+            }`}
+            src={local.book.image.url}
+            alt={local.book.image.alt}
+          />
+          <Show when={local.active}>
+            <img
+              class='md:hidden absolute flex w-[50vw] max-w-[260px] top-0 z-0 blur-2xl'
+              src={local.book.image.url}
+            />
+          </Show>
+        </div>
         <div class='flex items-center justify-between'>
           <div class='flex text-left items-center'>
             <TextScambler
               className={`${
                 local.active ? " bg-black text-white ml-0 px-2" : ""
-              } text-xl lg:text-[24px]`}
+              } text-xl md:text-[20px] lg:text-[24px]`}
               text={local.book.title}
             ></TextScambler>
           </div>
@@ -42,8 +54,8 @@ export const Card: Component<BookProps> = (props) => {
           </Show>
           <Show when={!!local.book.review}>
             <p
-              class={`text-white bg-black py-2 px-4 h-fit transition-all ${
-                local.active ? "-translate-x-2 lg:-translate-x-5" : ""
+              class={`text-white bg-black py-1 md:py-2 px-2 md:px-4 h-fit transition-all ${
+                local.active ? " md:-translate-x-5" : ""
               }`}
             >
               {local.book.review}/5{" "}
@@ -54,12 +66,12 @@ export const Card: Component<BookProps> = (props) => {
           {local.book.author}
         </p>
         <Show when={local.book.comment?.length}>
-          <p class='text-gray-700  '>{local.book.comment}</p>
+          <p class='text-gray-700 '>{local.book.comment}</p>
         </Show>
       </div>
       <Show when={local.active}>
         <div
-          class={`hidden lg:flex fixed left-[20vw]  items-center h-screen top-0`}
+          class={`hidden md:flex fixed left-[20vw]  items-center h-screen top-0`}
         >
           <CoverCard img={local.book.image} />
         </div>
